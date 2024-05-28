3724d5dbc5fde806198b9051d70879adfc29bd7b
Martu <143520216+MartuPe@users.noreply.github.com>
2024-05-28 20:51:52 -0300

    .

--- models/RegistrarModel.php
+++ models/RegistrarModel.php
@@ -1,42 +1,54 @@
 <?php
 
 require_once "vendor/PHPMailer-6.9.1/src/PHPMailer.php";
+
 class RegistrarModel
 {
     private $baseDeDatos;
+
     public function __construct($baseDeDatos)
     {
         $this->baseDeDatos = $baseDeDatos;
     }
 
-    public function registrarse($nombreUsuario,$nombre,$apellido,$email,$password,$pais,$ciudad,$fotoDePerfil,$añoNacimiento,$genero){
+    public function registrarse($nombreUsuario, $nombre, $apellido, $email, $password, $pais, $ciudad, $fotoDePerfil, $añoNacimiento, $genero)
+    {
 
         $rol = "usuario";
-
+        $nombre_foto = "";
         $fotoPerfil = null;
-        if(isset($fotoDePerfil)){
-            $errors= array();
+        if (isset($fotoDePerfil)) {
+            $errors = array();
             $file_name = $fotoDePerfil['name'];
             $file_size = $fotoDePerfil['size'];
             $file_tmp = $fotoDePerfil['tmp_name'];
             $file_type = $fotoDePerfil['type'];
 
-            $extensions= array("jpeg","jpg","png");
+            $extensions = array("jpeg", "jpg", "png");
 
-            if($file_size > 2097152){
-                $errors[]='File size must be less than 2 MB';
+            if ($file_size > 2097152) {
+                $errors[] = 'File size must be less than 2 MB';
             }
 
-            if(empty($errors)==true){
-                $fotoPerfil = file_get_contents($file_tmp);
-            }else{
+            if (empty($errors) == true) {
+                $fotoPerfil = $fotoDePerfil;
+                $archivo_temporal = $fotoDePerfil['tmp_name'];
+                $directorio_destino = '/public/img';
+                $nombre_foto = $directorio_destino . $fotoPerfil;
+                move_uploaded_file($archivo_temporal, $directorio_destino . $fotoPerfil);
+            } else {
                 print_r($errors);
             }
         }
 
-        $sql = "INSERT INTO usuario (nombre_de_usuario,nombre,apellido,email,password,pais,ciudad,foto,año_nacimiento,genero,rol) VALUES ($nombreUsuario,$nombre,$apellido,$email,$password,$pais,$ciudad,$fotoPerfil,$añoNacimiento,$genero,$rol)";
+        $sql = "INSERT INTO usuario (nombre_de_usuario,nombre,apellido,email,password,pais,ciudad,año_nacimiento,genero,rol, foto) VALUES ($nombreUsuario,$nombre,$apellido,$email,$password,$pais,$ciudad,$añoNacimiento,$genero,$rol,$nombre_foto)";
+
+        include_once ("helper/Logger.php");
+        $log = new Logger();
+        $log->info($sql);
 
         $this->baseDeDatos->query($sql);
+
 
         $hash = hash('sha256', $email . $password);
 
