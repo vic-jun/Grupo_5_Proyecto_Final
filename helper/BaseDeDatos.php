abf7cf8e9b4a3ce2a58e1411f5340bd178851340
Chud50 <pablostadtner@hotmail.com>
2024-05-28 17:09:10 -0300

    nose

---
+++ helper/BaseDeDatos.php
@@ -0,0 +1,30 @@
+<?php
+
+class BaseDeDatos
+{
+    private $baseDeDatos;
+
+    public function __construct($servername, $user, $dbname, $password)
+    {
+        $this->baseDeDatos = new mysqli($servername, $user, $dbname, $password);
+
+        if ($this->baseDeDatos->connect_error){
+            echo "Error: " . $this->baseDeDatos->connect_error;
+        }
+    }
+
+    public function query($sql){
+        $result = mysqli_query($this->baseDeDatos, $sql);
+        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+
+    public function execute($sql)
+    {
+        mysqli_query($this->baseDeDatos, $sql);
+    }
+
+    public function __destruct()
+    {
+        mysqli_close($this->baseDeDatos);
+    }
+}
